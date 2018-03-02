710a21b68d1b1cc648276b9bbf39e9e2eb7d8f73
mengmeiqi <mengmeiqi0727@sina.com>
2018-03-02 22:55:06 +0800

    PHP

--- demo10.php
+++ demo10.php
@@ -1,7 +1,17 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: dell
- * Date: 2018/3/2
- * Time: 22:54
+ * User: apple
+ * Date: 17/12/23
+ * Time: 上午9:23
  */
+
+//$cars=array('a'=>"Volvo",0=>'ccc',2=>'dddd','b'=>"BMW","SAAB");
+//var_dump($cars);
+
+$age=array("Bill"=>"35","Steve"=>"37","Peter"=>"43");
+
+foreach($age as $key=>$x_value) {
+    echo "Value=" . $x_value.",key=".$key;
+    echo "<br>";
+}
